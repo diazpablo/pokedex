7b9a9dd85811603d6b74747a0f648715df63b56b
Pablo Diaz <diazpablo@gmail.com>
2021-02-27 15:53:29 +0100

    improve on PokemonSelector (scollbar)

--- components/PokemonSelector.tsx
+++ components/PokemonSelector.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { RADIUS } from '../ui/settings';
+import { BUTTON, LIGHT, RADIUS } from '../ui/settings';
 
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 5px;
   height: 100%;
+  padding: 0 1rem;
   overflow-y: auto;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  /* Track */
+
+  ::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background: rgba(0, 0, 0, .4);
+    cursor: pointer;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: ${ LIGHT };
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: ${ BUTTON };
+  }
 `;
 
 const PokemonButton = styled.button.attrs({
