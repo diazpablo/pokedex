c81f55c78bc6c624d21b31c12bfa01f7417df509
Pablo Diaz <diazpablo@gmail.com>
2021-02-27 13:08:10 +0100

    improve on PokemonSelector (hover and selected state)

--- components/PokemonSelector.tsx
+++ components/PokemonSelector.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { RADIUS } from '../ui/settings';
 
 const Wrapper = styled.div`
   display: grid;
@@ -9,21 +10,31 @@ const Wrapper = styled.div`
   overflow-y: auto;
 `;
 
-const PokemonButton = styled.button`
-  background: none;
-  outline: none;
-  border: none;
-  box-shadow: none;
-  cursor: pointer;
+const PokemonButton = styled.button.attrs({
+  type: 'button'
+})`
+  background-color: rgba(0, 0, 0, 0);
+  border-radius: ${ RADIUS };
+  padding: 1rem .5rem;
+  transition: background-color .4s;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, .6);
+  }
+
+  ${ props => props.selected && css`
+    background-color: rgba(0, 0, 0, .35);
+  ` }
 `;
 
-const PokemonSelector = ({ pokemons, onSelect }) => {
+const PokemonSelector = ({ pokemons, selected, onSelect }) => {
   return (
     <Wrapper>
       {
         pokemons && pokemons.map(p => (
           <PokemonButton
             key={ p.num }
+            selected={ p.num === selected }
             onClick={ () => onSelect(p.num) }
           >
             <img src={ p.img } />
