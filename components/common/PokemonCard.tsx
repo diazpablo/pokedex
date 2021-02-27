2a265d1504cbb854d2ff9078694a986ee5ccc638
Pablo Diaz <diazpablo@gmail.com>
2021-02-27 12:14:13 +0100

    added styles to PokemonCard

--- components/common/PokemonCard.tsx
+++ components/common/PokemonCard.tsx
@@ -1,5 +1,44 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
+import { DARK, DARK_BLUE, LIGHT_BLUE, MEDIUM, RADIUS } from '../../ui/settings';
+
+const CardWrapper = styled.div`
+  padding: 2rem;
+  border: 1px solid #fff;
+  border-radius: ${ RADIUS };
+  background: ${ DARK_BLUE };
+  background: linear-gradient(135deg, ${ DARK_BLUE } 0%, ${ LIGHT_BLUE } 100%);
+  color: ${ DARK };
+  margin-right: 3rem;
+`;
+
+const PokeImg = styled.img`
+  margin: 0 auto -30px;
+  height: 150px;
+`;
+
+const PokeName = styled.h2`
+  text-align: center;
+  margin-bottom: .5rem;
+`;
+
+const PokeTypes = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: .5rem;
+`;
+
+const PokeType = styled.span`
+  margin: 0 5px;
+  padding: 2px 8px;
+  border: 1px solid ${ MEDIUM };
+  border-radius: ${ RADIUS };
+  color: ${ MEDIUM };
+  font-size: 0.7em;
+  font-weight: 600;
+  text-transform: uppercase;
+`;
 
 const GET_POKEMON = gql`
   query Pokemon($num: String!) {
@@ -25,7 +64,7 @@ type PokemonCardProps = {
   pokemonNum: string
 }
 
-const PokemonCard = ({ pokemonNum }: PokemonCardProps) => {
+const PokemonCard = ({ pokemonNum, ...rest }: PokemonCardProps) => {
   const { loading, error, data } = useQuery(GET_POKEMON, {
     variables: { num: pokemonNum }
   });
@@ -36,15 +75,17 @@ const PokemonCard = ({ pokemonNum }: PokemonCardProps) => {
   const { pokemon } = data;
 
   return (
-    <div>
-      <img src={ pokemon.img } alt={ pokemon.name } />
-      <h3>{ pokemon.name }</h3>
+    <CardWrapper { ...rest }>
+      <PokeImg src={ pokemon.img } alt={ pokemon.name } />
 
-      <p><strong>Types:</strong>{ pokemon.type.map(t => <small key={ t }>{ t }</small>) }</p>
-      <p><strong>Height:</strong>{ pokemon.height }</p>
-      <p><strong>Weight:</strong>{ pokemon.weight }</p>
-      <p><strong>Weaknesses:</strong>{ pokemon.weaknesses.map(w => <small key={ w }>{ w }</small>) }</p>
-    </div>
+      <div style={ { backgroundColor: 'white', padding: '2rem 1rem' } }>
+        <PokeName>{ pokemon.name }</PokeName>
+        <PokeTypes>{ pokemon.type.map(t => <PokeType key={ t }>{ t }</PokeType>) }</PokeTypes>
+        <p><strong>Height:</strong> { pokemon.height }</p>
+        <p><strong>Weight:</strong> { pokemon.weight }</p>
+        <p><strong>Weaknesses:</strong> { pokemon.weaknesses.join(', ') }</p>
+      </div>
+    </CardWrapper>
   );
 };
 
